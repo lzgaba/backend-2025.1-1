bbe3d1becb6f423cbb1bfd2611bb9b9a76887f2b
lzgaba <gabrieltorresdematos@gmail.com>
2025-05-31 15:29:44 -0300

    Luiz Gabriel At web.js

--- routes/web.js
+++ routes/web.js
@@ -1,29 +1,15 @@
-import express from "express";
-
-import NavegacaoController from "../controllers/NavegacaoController.js";
-import TasksController from "../controllers/TasksController.js";
-import UsersController from "../controllers/UsersController.js";
+import express from 'express';
+import TaskController from '../controllers/TaskController.js';
 
 const router = express.Router();
 
 // Rotas das Tarefas
-// Implementação de CRUD para tarefas
-// router.get('/tasks', TasksController.list);
-
-// Rotas dos Usuários
-router.get('/users', UsersController.list)
-router.get('/users/create', UsersController.create)
-router.post('/users/save', UsersController.save)
-router.post('/users/remove/:id', UsersController.remove)
-router.get('/users/edit/:id', UsersController.edit)
-router.post('/users/update', UsersController.update)
-router.post('/users/update-status/:id', UsersController.updateStatus)
-
-router.get('/', NavegacaoController.index);
-router.get('/sobre', NavegacaoController.sobre);
-
-router.get(/.*/, (req, res) => {
-  return res.render("404");
-});
+router.get('/tasks', TaskController.list);
+router.get('/tasks/create', TaskController.create);
+router.post('/tasks/save', TaskController.save);
+router.get('/tasks/edit/:id', TaskController.edit);
+router.post('/tasks/update', TaskController.update);
+router.post('/tasks/remove/:id', TaskController.remove);
+router.post('/tasks/update-status/:id', TaskController.updateStatus);
 
 export default router;
