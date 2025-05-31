8426e831ca0f15d6eb2259a9e84c44c835f59da0
lzgaba <gabrieltorresdematos@gmail.com>
2025-05-31 15:24:08 -0300

    Luiz Gabriel Torres de Matos TasksController.js

--- controllers/TasksController.js
+++ controllers/TasksController.js
@@ -1,45 +1,108 @@
 import Task from '../models/Task.js';
 
 function TaskController() {
-
-  async function list(req, res) {
-    // Implementação da listagem de tarefas
+  // Listar todas as tarefas
+  async function list(req, res, next) {
+    try {
+      const tasks = await Task.findAll({ raw: true });
+      res.render('tasks/list', { title: 'Lista de Tarefas', tasks });
+    } catch (err) {
+      next(err);
+    }
   }
 
+  // Exibir o formulário de criação de tarefa
   function create(req, res) {
-    // Implementação da criação de tarefas
+    res.render('tasks/create', { title: 'Criar Tarefa' });
   }
 
-  async function save(req, res) {
-    // Implementação do salvamento de tarefas
+  // Salvar uma nova tarefa
+  async function save(req, res, next) {
+    try {
+      const { name, description, dueDate } = req.body;
+      if (!name || !description) {
+        return res.render('tasks/create', {
+          title: 'Criar Tarefa',
+          error: { message: 'Nome e descrição são obrigatórios.' },
+        });
+      }
+
+      const task = { name, description, dueDate };
+      await Task.create(task);
+      res.redirect('/tasks');
+    } catch (err) {
+      next(err);
+    }
   }
 
-  async function edit(req, res) {
-    // Implementação da edição de tarefas
+  // Exibir o formulário de edição de tarefa
+  async function edit(req, res, next) {
+    try {
+      const task = await Task.findOne({ where: { id: req.params.id }, raw: true });
+      if (!task) {
+        return res.render('404', { title: 'Tarefa Não Encontrada' });
+      }
+      res.render('tasks/edit', { title: 'Editar Tarefa', task });
+    } catch (err) {
+      next(err);
+    }
   }
 
-  async function update(req, res) {
-    // Implementação da atualização de tarefas
+  // Atualizar uma tarefa existente
+  async function update(req, res, next) {
+    try {
+      const { name, description, dueDate } = req.body;
+      if (!name || !description) {
+        return res.render('tasks/edit', {
+          title: 'Editar Tarefa',
+          error: { message: 'Nome e descrição são obrigatórios.' },
+          task: req.body,
+        });
+      }
+
+      const task = { name, description, dueDate };
+      await Task.update(task, { where: { id: req.body.id } });
+      res.redirect('/tasks');
+    } catch (err) {
+      next(err);
+    }
   }
 
-  async function remove(req, res) {
-    // Implementação da remoção de tarefas
+  // Remover uma tarefa
+  async function remove(req, res, next) {
+    try {
+      await Task.destroy({ where: { id: req.params.id } });
+      res.redirect('/tasks');
+    } catch (err) {
+      next(err);
+    }
   }
 
-  async function updateStatus(req, res) {
-    // Implementação da atualização de status de tarefas
+  // Atualizar o status de uma tarefa
+  async function updateStatus(req, res, next) {
+    try {
+      const task = await Task.findOne({ where: { id: req.params.id } });
+      if (!task) {
+        return res.render('404', { title: 'Tarefa Não Encontrada' });
+      }
+
+      const updatedStatus = task.completed ? false : true;
+      await Task.update({ completed: updatedStatus }, { where: { id: req.params.id } });
+      res.redirect('/tasks');
+    } catch (err) {
+      next(err);
+    }
   }
 
   return {
+    list,
     create,
     save,
-    list,
-    remove,
     edit,
     update,
+    remove,
     updateStatus,
-  }
-
+  };
 }
 
 export default TaskController();
